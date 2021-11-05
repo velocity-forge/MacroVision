14d0364a94d40a2952f7c12c2f64ef085d8519f5
velocity-forge <velforge.tech28@gmail.com>
2021-11-05 16:24:11 -0400

    Upgrade to Next v12. Add Main layout component.

---
+++ source/layouts/Main/index.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+import { rem } from 'polished';
+
+const Main = styled('main')`
+  margin-left: auto;
+  margin-right: auto;
+  max-width: ${rem('1200px')};
+  padding-left: 1em;
+  padding-right: 1em;
+`;
+
+export default Main;
