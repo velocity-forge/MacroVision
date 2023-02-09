f03d613ba4105431db30fe0b8a9a4559e32e1965
velocity-forge <velforge.tech28@gmail.com>
2023-02-09 16:46:12 -0600

    Add text styles

---
+++ services/app/source/base/typography/text-styles.stories.tsx
@@ -0,0 +1,28 @@
+import { Meta, Story } from '@storybook/react';
+import styles from './text-styles.module.css';
+
+const settings: Meta = {
+  title: 'Global/Text Styles',
+};
+
+const TextStyles: Story = () => {
+  return (
+    <>
+      {Object.entries(styles).map(([name, style]) => (
+        <div key={name} style={{ marginBottom: '3rem' }}>
+          <div style={{ textTransform: 'uppercase' }}>{name}</div>
+          <div className={style as string}>
+            Alice was beginning to get very tired of sitting by her sister on
+            the bank, and of having nothing to do: once or twice she had peeped
+            into the book her sister was reading, but it had no pictures or
+            conversations in it, ‘and what is the use of a book,’ thought Alice
+            ‘without pictures or conversations?’
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default settings;
+export { TextStyles };
