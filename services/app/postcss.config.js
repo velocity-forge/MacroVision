be354578ceff52c18855c4df8196670035905339
velocity-forge <velforge.tech28@gmail.com>
2023-02-10 10:33:19 -0600

    Replace autoprefixer with postcss-preset-env

--- services/app/postcss.config.js
+++ services/app/postcss.config.js
@@ -1,7 +1,9 @@
 module.exports = {
   plugins: {
     'postcss-rem': {},
-    autoprefixer: {},
+    'postcss-preset-env': {
+      stage: 3,
+    },
     'postcss-advanced-variables': {},
     [require.resolve('./lib/responsive-font-size.js')]: {},
   },
