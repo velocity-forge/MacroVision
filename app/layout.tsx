820254143b6857c000c8667ba8b64e07bab6a5e8
velocity-forge <velforge.tech28@gmail.com>
2024-03-05 09:42:52 -0500

    Re-re-run Prettier over app directory

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import { JSX, PropsWithChildren } from 'react';
 import '../source/00-config/index.css';
-import '../source/01-global/index.css';
 import SourceSansFontStyle from '../source/01-global/fonts/source-sans';
-import Skiplink from '../source/03-components/Skiplink/Skiplink';
-import SiteContainer from '../source/02-layouts/SiteContainer/SiteContainer';
-import Header from '../source/02-layouts/Header/Header';
-import SiteName from '../source/03-components/SiteName/SiteName';
-import ResponsiveMenu from '../source/03-components/Menu/ResponsiveMenu/ResponsiveMenu';
+import '../source/01-global/index.css';
 import Footer from '../source/02-layouts/Footer/Footer';
-import Menu from '../source/03-components/Menu/Menu';
-import footerStyles from '../source/03-components/Menu/menu-footer.module.css';
+import Header from '../source/02-layouts/Header/Header';
+import SiteContainer from '../source/02-layouts/SiteContainer/SiteContainer';
 import BackToTop from '../source/03-components/BackToTop/BackToTop';
+import Menu from '../source/03-components/Menu/Menu';
+import ResponsiveMenu from '../source/03-components/Menu/ResponsiveMenu/ResponsiveMenu';
+import footerStyles from '../source/03-components/Menu/menu-footer.module.css';
+import SiteName from '../source/03-components/SiteName/SiteName';
+import Skiplink from '../source/03-components/Skiplink/Skiplink';
 import '../source/06-utility/index.css';
 
 function RootLayout({ children }: PropsWithChildren): JSX.Element {
