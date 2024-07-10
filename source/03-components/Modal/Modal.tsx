d68ee33317397b12b4e9f91bb372fc06d4f0f29c
velocity-forge <velforge.tech28@gmail.com>
2024-07-10 17:43:54 -0400

    Ensures component has proper cleanup and leverages more React functionality

--- source/03-components/Modal/Modal.tsx
+++ source/03-components/Modal/Modal.tsx
@@ -1,10 +1,18 @@
+'use client';
+
 import clsx from 'clsx';
 import { GessoComponent } from 'gesso';
-import { MouseEventHandler, useEffect, useRef } from 'react';
+import {
+  KeyboardEventHandler,
+  MouseEventHandler,
+  useEffect,
+  useId,
+  useRef,
+} from 'react';
 import styles from './modal.module.css';
 
 interface ModalProps extends GessoComponent {
-  id: string;
+  id?: string;
   title?: string;
   children: React.ReactNode;
   defaultOpen?: boolean;
@@ -18,27 +26,69 @@ function Modal({
   modifierClasses,
 }: ModalProps): JSX.Element {
   const dialogRef = useRef<HTMLDialogElement>(null);
+  const generatedId = useId();
+  const modalId = id || generatedId;
 
   useEffect(() => {
     if (defaultOpen) {
       openModal();
     }
 
-    handleKeyDown();
-
     const modalOpenButtons = document.querySelectorAll(
-      `[aria-controls="modal-${id}"]`,
+      `[aria-controls="modal-${modalId}"]`,
     );
     modalOpenButtons.forEach(button => {
       button.addEventListener('click', openModal);
     });
-  });
+
+    // Cleanup
+    return () => {
+      modalOpenButtons.forEach(button => {
+        button.removeEventListener('click', openModal);
+      });
+    };
+  }, [defaultOpen, modalId]);
 
   // Close modal on overlay click
   const handleOverlayClick: MouseEventHandler<HTMLDialogElement> = event => {
     const target = event.target as HTMLElement;
-    if (target && target.closest && target.closest(`dialog > div`)) return;
+    if (target && target.closest && target.closest(`.${styles.inner}`)) return;
     closeModal();
+  };
+
+  // Function to handle key downs while modal is open
+  const handleKeyDown: KeyboardEventHandler<HTMLDialogElement> = event => {
+    const modal = dialogRef.current;
+    if (!modal) return;
+
+    const focusableElements = modal.querySelectorAll(
+      'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select',
+    );
+    const firstFocusableElement = focusableElements[0] as HTMLElement;
+    const lastFocusableElement = focusableElements[
+      focusableElements.length - 1
+    ] as HTMLElement;
+
+    if (event.key === 'Tab') {
+      // If shift key pressed for shift + tab combination
+      if (event.shiftKey) {
+        if (document.activeElement === firstFocusableElement) {
+          // Add focus for the last focusable element
+          lastFocusableElement.focus();
+          event.preventDefault();
+        }
+      }
+      // If focused has reached to last focusable element then focus first focusable element after pressing tab
+      else if (document.activeElement === lastFocusableElement) {
+        // Add focus for the first focusable element
+        firstFocusableElement.focus();
+        event.preventDefault();
+      }
+    } else if (event.key === 'Escape') {
+      // Close modal on escape key press
+      event.preventDefault();
+      closeModal();
+    }
   };
 
   // Function to show modal
@@ -69,63 +119,28 @@ function Modal({
     modal.dispatchEvent(event);
   };
 
-  // Function to handle key downs while modal is open
-  const handleKeyDown = () => {
-    const modal = dialogRef.current;
-    if (!modal) return;
-
-    const focusableElements = modal.querySelectorAll(
-      'a[href], button, textarea, input[type="text"], input[type="radio"], input[type="checkbox"], select',
-    );
-    const firstFocusableElement = focusableElements[0] as HTMLElement;
-    const lastFocusableElement = focusableElements[
-      focusableElements.length - 1
-    ] as HTMLElement;
-
-    modal.addEventListener('keydown', e => {
-      if (e.key === 'Tab') {
-        // If shift key pressed for shift + tab combination
-        if (e.shiftKey) {
-          if (document.activeElement === firstFocusableElement) {
-            // Add focus for the last focusable element
-            lastFocusableElement.focus();
-            e.preventDefault();
-          }
-        }
-        // If focused has reached to last focusable element then focus first focusable element after pressing tab
-        else if (document.activeElement === lastFocusableElement) {
-          // Add focus for the first focusable element
-          firstFocusableElement.focus();
-          e.preventDefault();
-        }
-      } else if (e.key === 'Escape') {
-        // Close modal on escape key press
-        e.preventDefault();
-        closeModal();
-      }
-    });
-  };
-
   return (
     <dialog
       ref={dialogRef}
       className={clsx(styles.modal, modifierClasses)}
-      id={`modal-${id}`}
-      aria-labelledby={`modal-title-${id}`}
-      aria-describedby={`modal-content-${id}`}
+      id={`modal-${modalId}`}
+      aria-labelledby={`modal-title-${modalId}`}
+      aria-describedby={`modal-content-${modalId}`}
       onClick={handleOverlayClick}
+      onKeyDown={handleKeyDown}
     >
       <div className={styles.inner}>
         <button
+          type="button"
           className={styles.closeButton}
           aria-label="Close modal"
-          aria-controls={`modal-${id}`}
+          aria-controls={`modal-${modalId}`}
           autoFocus
           onClick={closeModal}
         ></button>
-        <div className={styles.content} id={`modal-content-${id}`}>
+        <div className={styles.content} id={`modal-content-${modalId}`}>
           {title && (
-            <h2 className={styles.title} id={`modal-title-${id}`}>
+            <h2 className={styles.title} id={`modal-title-${modalId}`}>
               {title}
             </h2>
           )}
